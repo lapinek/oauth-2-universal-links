a39dea287075d158a70dd76c0d6124a39b848266
Konstantin Lapine <konstantin.lapine@forgerock.com>
2019-05-23 12:31:56 -0700

    initial

---
+++ oauth-2-universal-links/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  oauth-2-universal-links
+//
+//  Created by Konstantin Lapine on 5/23/19.
+//  Copyright © 2019 Forgerock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
